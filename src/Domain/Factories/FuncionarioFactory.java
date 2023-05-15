d6df62d5c0ec8d6d8e62fbc6f1e85303b1239855
João Macêdo <joaomacedx@gmail.com>
2023-05-15 20:48:18 -0300

    feat: create methods to funcionario factory

--- src/Domain/Factories/FuncionarioFactory.java
+++ src/Domain/Factories/FuncionarioFactory.java
@@ -1,5 +1,30 @@
 package Domain.Factories;
 
+import java.util.UUID;
+
+import Domain.Entities.Funcionario;
+import Domain.ValueObjects.Cargo;
+import Domain.ValueObjects.Email;
+import Domain.ValueObjects.Endereco;
+import Domain.ValueObjects.Nome;
+import Domain.ValueObjects.NumeroDeTelefone;
+
 public class FuncionarioFactory {
-    
+
+    public Funcionario criarNovoFuncionario( String nome, String sobrenome, String numeroDeTelefone,
+     String enderecoDeEmail, String rua, String cep, Cargo cargo ) {
+        Funcionario novoFuncionario = new Funcionario(UUID.randomUUID(),
+            new Nome(nome, sobrenome), cargo, new Email(enderecoDeEmail),
+            new NumeroDeTelefone(numeroDeTelefone), new Endereco(rua, cep));
+
+        return novoFuncionario;
+    }
+    public Funcionario criarFuncionarioExistente(UUID id, String nome, String sobrenome, String numeroDeTelefone,
+    String enderecoDeEmail, String rua, String cep, Cargo cargo ) {
+       Funcionario funcionarioExistente = new Funcionario(id,
+           new Nome(nome, sobrenome), cargo, new Email(enderecoDeEmail),
+           new NumeroDeTelefone(numeroDeTelefone), new Endereco(rua, cep));
+
+       return funcionarioExistente;
+   }
 }
--- src/Domain/ValueObjects/Endereco.java
+++ src/Domain/ValueObjects/Endereco.java
@@ -2,13 +2,9 @@ package Domain.ValueObjects;
 
 public class Endereco {
     public String rua;
-    public Integer numero; 
     public String cep; 
-    public String complemento;
-    public Endereco(String rua, Integer numero, String cep, String complemento) {
+    public Endereco(String rua, String cep) {
         this.rua = rua;
-        this.numero = numero;
         this.cep = cep;
-        this.complemento = complemento;
     }
 }
