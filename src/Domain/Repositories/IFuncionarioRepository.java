cce735349a16c3a34d746a0d6f4cc53967288791
João Macêdo <joaomacedx@gmail.com>
2023-05-15 20:54:58 -0300

    feat: create methods in repository

--- src/Domain/Repositories/IFuncionarioRepository.java
+++ src/Domain/Repositories/IFuncionarioRepository.java
@@ -1,6 +1,13 @@
  package Domain.Repositories;
- 
- public interface IFuncionarioRepository {
 
-    
+import java.util.UUID;
+
+import Domain.Entities.Funcionario;
+import Domain.ValueObjects.Email;
+
+public interface IFuncionarioRepository {
+    public Funcionario findByEmail(Email email);
+    public void Save(Funcionario novoFuncionario);
+    public Funcionario findById(UUID idDoFuncionario);
+    public void Delete(Funcionario funcionarioParaDeletar);   
 }
--- src/Infrastructure/Repositories/InMemory/InMemoryFuncionarioRepository.java
+++ src/Infrastructure/Repositories/InMemory/InMemoryFuncionarioRepository.java
@@ -1,7 +1,28 @@
 package Infrastructure.Repositories.InMemory;
 
+import java.util.UUID;
+
+import Domain.Entities.Funcionario;
 import Domain.Repositories.IFuncionarioRepository;
+import Domain.ValueObjects.Email;
 
 public class InMemoryFuncionarioRepository implements IFuncionarioRepository {
+
+    public Funcionario findByEmail(Email email) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByEmail'");
+    }
+    public void Save(Funcionario novoFuncionario) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Save'");
+    }
+    public Funcionario findById(UUID idDoFuncionario) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+    public void Delete(Funcionario funcionarioParaDeletar) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Delete'");
+    }
     
 }
