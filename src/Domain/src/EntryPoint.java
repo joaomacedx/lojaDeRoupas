342256a8e93b816e0f18cb634a0d5b0c193fbb52
João Macêdo <joaomacedx@gmail.com>
2023-05-15 19:00:59 -0300

    feat: create projects structure

---
+++ src/Domain/src/EntryPoint.java
@@ -0,0 +1,7 @@
+package Domain.src;
+
+public class EntryPoint {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/Infrastructure/src/App.java
@@ -0,0 +1,7 @@
+package Infrastructure.src;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
