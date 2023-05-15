25163930a06bacc04cf9ecd7a9d6e14c6957cd4e
João Macêdo <joaomacedx@gmail.com>
2023-05-15 19:35:51 -0300

    feat: init entity funcionario implementation

--- src/Domain/Entities/Funcionario.java
+++ src/Domain/Entities/Funcionario.java
@@ -1,5 +1,15 @@
 package Domain.Entities;
+import java.util.UUID;
+
+
+import Domain.ValueObjects.Nome;
 
 public class Funcionario {
-    
+    private UUID Id;
+    private Nome nome;
+
+    public Funcionario(UUID id, Nome nome) {
+        this.Id = id;
+        this.nome = nome;
+    }
 }
--- src/Domain/ValueObjects/Nome.java
+++ src/Domain/ValueObjects/Nome.java
@@ -1,5 +1,11 @@
 package Domain.ValueObjects;
 
 public class Nome {
-    
+    private String nome;
+    private String sobrenome;
+
+    public Nome(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
 }
