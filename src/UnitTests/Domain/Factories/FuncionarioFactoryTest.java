db88cf038373241d1791d138fb16b6372020edec
João Macêdo <joaomacedx@gmail.com>
2023-05-16 18:04:31 -0300

    test: create method to test criarFuncionarExistente

--- src/UnitTests/Domain/Factories/FuncionarioFactoryTest.java
+++ src/UnitTests/Domain/Factories/FuncionarioFactoryTest.java
@@ -1,4 +1,6 @@
 package UnitTests.Domain.Factories;
+import java.util.UUID;
+
 import org.junit.Test;
 
 import Domain.Entities.Funcionario;
@@ -6,7 +8,7 @@ import Domain.Factories.FuncionarioFactory;
 import Domain.ValueObjects.*;
 public class FuncionarioFactoryTest {
     @Test
-    public void criaNovo_com_sucesso_QUANDO_argumentos_passados_sao_validos_ENTAO_retorna_nova_instancia_de_Funcionario() {
+    public void criarNovo_com_sucesso_QUANDO_argumentos_passados_sao_validos_ENTAO_retorna_nova_instancia_de_Funcionario() {
 
         //Arrange
         String nomeDoFuncionario = "Gabriel";
@@ -22,7 +24,34 @@ public class FuncionarioFactoryTest {
         Funcionario funcionarioAtual = funcionarioFactory.criarNovoFuncionario(nomeDoFuncionario, sobrenomeDoFuncionario,
              telefone, email, ruaOndeFuncionarioMora,
               cepDaRuaDoFuncionario, cargoDoFuncionario);
-              
+
+        //Assert
+        Funcionario funcionarioEsperado = new Funcionario(funcionarioAtual.Id,
+             new Nome(nomeDoFuncionario, sobrenomeDoFuncionario),
+             cargoDoFuncionario, new Email(email), new NumeroDeTelefone(telefone),
+              new Endereco(ruaOndeFuncionarioMora,
+               cepDaRuaDoFuncionario));
+        funcionarioAtual.equals(funcionarioEsperado);
+    }
+    @Test
+    public void criarExistente_com_sucesso_QUANDO_argumentos_passados_sao_validos_ENTAO_retorna_nova_instancia_de_Funcionario() {
+
+        //Arrange
+        UUID id = UUID.randomUUID();
+        String nomeDoFuncionario = "Gabriel";
+        String sobrenomeDoFuncionario = "Macedo";
+        String telefone = "12345678";
+        String email = "joaomacedo1234@email.com";
+        String ruaOndeFuncionarioMora = "Rua das babilonia";
+        String cepDaRuaDoFuncionario = "987654321";
+        Cargo cargoDoFuncionario = Cargo.Gerente;
+        FuncionarioFactory funcionarioFactory = new FuncionarioFactory();
+
+        //Act
+        Funcionario funcionarioAtual = funcionarioFactory.criarFuncionarioExistente(id,nomeDoFuncionario, sobrenomeDoFuncionario,
+             telefone, email, ruaOndeFuncionarioMora,
+              cepDaRuaDoFuncionario, cargoDoFuncionario);
+
         //Assert
         Funcionario funcionarioEsperado = new Funcionario(funcionarioAtual.Id,
              new Nome(nomeDoFuncionario, sobrenomeDoFuncionario),
