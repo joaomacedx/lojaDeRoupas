e22b97ba7be60d43c9443fbceeac6a9c88977e3a
João Macêdo <joaomacedx@gmail.com>
2023-05-16 15:41:51 -0300

    test: create method to test criaNovo method in FuncionarioFactory

--- src/UnitTests/Domain/Factories/FuncionarioFactoryTest.java
+++ src/UnitTests/Domain/Factories/FuncionarioFactoryTest.java
@@ -1,8 +1,32 @@
 package UnitTests.Domain.Factories;
 import org.junit.Test;
+
+import Domain.Entities.Funcionario;
+import Domain.Factories.FuncionarioFactory;
+import Domain.ValueObjects.*;
 public class FuncionarioFactoryTest {
     @Test
-    public void criaNovo_sucesso_QUANDO_argumentos_sao_validos_ENTAO_retorna_nova_instancia_de_Funcionario() {
+    public void criaNovo_com_sucesso_QUANDO_argumentos_passados_sao_validos_ENTAO_retorna_nova_instancia_de_Funcionario() {
 
+        //Arrange
+        String nomeDoFuncionario = "Gabriel";
+        String sobrenomeDoFuncionario = "Macedo";
+        String telefone = "12345678";
+        String email = "joaomacedo1234@email.com";
+        String ruaOndeFuncionarioMora = "Rua das babilonia";
+        String cepDaRuaDoFuncionario = "987654321";
+        Cargo cargoDoFuncionario = Cargo.Gerente;
+        FuncionarioFactory funcionarioFactory = new FuncionarioFactory();
+        //Act
+        Funcionario funcionarioAtual = funcionarioFactory.criarNovoFuncionario(nomeDoFuncionario, sobrenomeDoFuncionario,
+             telefone, email, ruaOndeFuncionarioMora,
+              cepDaRuaDoFuncionario, cargoDoFuncionario);
+        //Assert
+        Funcionario funcionarioEsperado = new Funcionario(funcionarioAtual.Id,
+             new Nome(nomeDoFuncionario, sobrenomeDoFuncionario),
+             cargoDoFuncionario, new Email(email), new NumeroDeTelefone(telefone),
+              new Endereco(ruaOndeFuncionarioMora,
+               cepDaRuaDoFuncionario));
+        funcionarioAtual.equals(funcionarioEsperado);
     }
 }
