3eaadf9ec117b6ad2d552e54d8bc6fa2ae8e8f51
João Macêdo <joaomacedx@gmail.com>
2023-05-15 21:25:23 -0300

    test: create criaNovo test for FuncionarioFactory

--- src/UnitTests/Domain/Factories/FuncionarioFactoryTest.java
+++ src/UnitTests/Domain/Factories/FuncionarioFactoryTest.java
@@ -1,6 +1,8 @@
 package UnitTests.Domain.Factories;
-
+import org.junit.Test;
 public class FuncionarioFactoryTest {
+    @Test
+    public void criaNovo_sucesso_QUANDO_argumentos_sao_validos_ENTAO_retorna_nova_instancia_de_Funcionario() {
 
-    
+    }
 }
