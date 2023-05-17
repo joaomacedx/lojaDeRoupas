9700168e7013dfae01cd6f171556f33ca6ad7ec6
João Macêdo <joaomacedx@gmail.com>
2023-05-16 22:38:35 -0300

    test: implement test for Delete funcionario and return error when funcionario is not saved

--- src/UnitTests/Infrastructure/InMemory/InMemoryFuncionarioRepositoryTest.java
+++ src/UnitTests/Infrastructure/InMemory/InMemoryFuncionarioRepositoryTest.java
@@ -176,4 +176,30 @@ public class InMemoryFuncionarioRepositoryTest {
         Boolean containsFuncionario = inMemoryFuncionarios.funcionariosInMemory.contains(funcionarioAtual);
         assertEquals(containsFuncionario, false);
     }
+    @Test(expected = Error.class)
+    public void Delete_com_erro_QUANDO_funcionario_passado_nao_esta_cadastrado__ENTAO_retorna_um_Error() {
+        //Arrange
+
+        UUID id = UUID.randomUUID();
+        String nomeDoFuncionario = "Gabriel";
+        String sobrenomeDoFuncionario = "Macedo";
+        String telefone = "12345678";
+        String email = "joaomacedo1234@email.com";
+        String ruaOndeFuncionarioMora = "Rua das babilonia";
+        String cepDaRuaDoFuncionario = "987654321";
+        Cargo cargoDoFuncionario = Cargo.Gerente;
+        Funcionario funcionario = new Funcionario(id,
+             new Nome(nomeDoFuncionario, sobrenomeDoFuncionario),
+             cargoDoFuncionario, new Email(email), new NumeroDeTelefone(telefone),
+             new Endereco(ruaOndeFuncionarioMora,
+             cepDaRuaDoFuncionario));
+        List<Funcionario> funcionarios = new ArrayList<Funcionario>();
+        InMemoryFuncionarioRepository inMemoryFuncionarios = new InMemoryFuncionarioRepository(funcionarios);
+
+        //Act
+        inMemoryFuncionarios.Delete(funcionario);
+        //Assert
+        Boolean containsFuncionario = inMemoryFuncionarios.funcionariosInMemory.contains(funcionario);
+        assertEquals(containsFuncionario, false);
+    }
 }
