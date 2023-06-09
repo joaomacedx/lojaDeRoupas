69ce6eb54f368c4c477ae086708d7735d2ba984a
João Macêdo <joaomacedx@gmail.com>
2023-06-08 23:49:19 -0300

    build: finish maven configuration

---
+++ src/WebAPI/src/test/br/com/lojaDeRoupas/APITestes/WebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lojaDeRoupas.APITestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
